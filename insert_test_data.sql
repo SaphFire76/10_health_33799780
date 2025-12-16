850ce7f0e305604551144df18bff7fadacfab4f9
Abrar <araha002@gold.ac.uk>
2025-12-16 21:08:10 +0000

    Insert Data SQL Changed

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -2,5 +2,5 @@
 
 USE health;
 
-INSERT INTO patients (username, fname, lname, email, hashedPass) VALUES ('gold', 'gold', 'smiths', 'gold', '$2a$12$.EaBJNIJTapGeiZX0lf9lOslMKghnM4vXdNzOpAeruKEvH5eRzMJu');
-INSERT INTO staff (username, fname, lname, email, hashedPass) VALUES ('gold', 'gold', 'smiths', 'gold', '$2a$12$.EaBJNIJTapGeiZX0lf9lOslMKghnM4vXdNzOpAeruKEvH5eRzMJu');
+INSERT INTO patients (username, fname, lname, email, hashedPass) VALUES ('gold', 'gold', 'smiths', 'goldsmiths@example.com', '$2a$12$V8pLNwpzqV1DHMu3hmJcGucVsGXfHxnxXlls4PkAxCa3pzfhUFJyG');
+INSERT INTO staff (username, fname, lname, email, hashedPass) VALUES ('gold', 'gold', 'smiths', 'goldsmiths@example.com', '$2a$12$V8pLNwpzqV1DHMu3hmJcGucVsGXfHxnxXlls4PkAxCa3pzfhUFJyG');
