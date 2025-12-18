f26934ed65e1dabb0fed4a5acbea1d597f17a549
Abrar <araha002@gold.ac.uk>
2025-12-18 23:56:55 +0000

    Update report content and enhance SQL test data with additional appointment entries and links

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -4,3 +4,5 @@ USE health;
 
 INSERT INTO patients (username, fname, lname, email, hashedPass) VALUES ('gold', 'gold', 'smiths', 'goldsmiths@example.com', '$2a$12$Uard57IvpGC9ExjMApUEte6zX7ZM05g6NW0IQouz8DbXvR1P3Ie0m');
 INSERT INTO staff (username, fname, lname, email, hashedPass) VALUES ('gold', 'gold', 'smiths', 'goldsmiths@example.com', '$2a$12$Uard57IvpGC9ExjMApUEte6zX7ZM05g6NW0IQouz8DbXvR1P3Ie0m');
+INSERT INTO appointments (patientID, slot, reason) VALUES (1, '2025-02-01 10:00:00', 'General Checkup');
+INSERT INTO appointments (patientID, slot, reason) VALUES (1, '2024-02-01 10:00:00', 'General Checkup');
